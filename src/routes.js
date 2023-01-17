5d4ba22f7aaf9e6fe027940697cb723f7452799d
Daniel <ldaniel.eng@gmail.com>
2023-01-17 15:02:22 -0300

    feat: funcao Delete

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import {Database} from './database.js'
 import {randomUUID} from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
@@ -7,7 +8,7 @@ const database = new Database()
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) =>{
       const users = database.select('users')
 
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const {name,email } = req.body
 
@@ -31,5 +32,17 @@ export const routes = [
       return res.writeHead(201).end()
 
     }
-  }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath(`/users/:id`),
+    handler: (req, res) => {
+
+      const{id}=req.params
+
+      database.delete('users', id)
+     
+      return res.writeHead(204).end()
+    }
+  },
 ]
